a7d4b89f6db23d3cc907be4dfa3bac8f70e1d2ba
XQ_IOS_MAN <917709989@qq.com>
2022-11-29 23:36:13 +0800

    1.1.95

--- src/utils/xq-type-script-framework/index.ts
+++ src/utils/xq-type-script-framework/index.ts
@@ -14,7 +14,7 @@
  * XQTypeScriptFramework 
  */
 import "./object/index"    // 类 工具类
-// import "./string/index"    // 字符串 工具类
+import "./string/index"    // 字符串 工具类
 import "./array/index"     // 数组 工具类
-// import "./date/index"      // 日期 工具类
-// import "./number/index"      // 日期 工具类
+import "./date/index"      // 日期 工具类
+import "./number/index"      // 日期 工具类
